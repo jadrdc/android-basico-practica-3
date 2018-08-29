3fe8f462edf743b097c20252cdaa412052d978fc
Jadrdc <jadrdc@gmail.com>
2018-08-29 00:13:57 -0400

    First Commit

---
+++ app/build/generated/source/buildConfig/debug/com/example/agustinreinoso/practica3altice/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.agustinreinoso.practica3altice;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.agustinreinoso.practica3altice";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
